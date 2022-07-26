4f805f173bee944740af6a20f3d95b2d0e102398
Amir <amir.buchali@gmail.com>
2022-07-27 01:48:56 +0430

    Add: test Generator and Critic

--- src/wgan/Critic.py
+++ src/wgan/Critic.py
@@ -1,4 +1,7 @@
+import torch
 from torch import nn
+
+
 
 class Critic(nn.Module):
     """
@@ -8,23 +11,25 @@ class Critic(nn.Module):
         img_chan (int): the number of the input image channels
         hidden_dim (int): the number of the hidden units
     """
-    def __init__(self, img_chan=1, hidden_dim=64):
+    def __init__(self, img_chan=1, hidden_dim=32):
         super(Critic, self).__init__()
 
         # The Network
         self.critic = nn.Sequential(
             self.make_critic_block(img_chan, hidden_dim),
             self.make_critic_block(hidden_dim, 2*hidden_dim),
-            self.make_critic_block(2*hidden_dim, img_chan, final_layer=True)
+            self.make_critic_block(2*hidden_dim, 4*hidden_dim),
+            self.make_critic_block(4*hidden_dim, 8*hidden_dim),
+            self.make_critic_block(8*hidden_dim, img_chan, kernel_size=2, padding=0, final_layer=True)
         )
 
-    def make_critic_block(self, input_dim, output_chan, kernel_size=4, stride=2, padding=1, final_layer=False):
+    def make_critic_block(self, input_dim, output_dim, kernel_size=4, stride=2, padding=1, final_layer=False):
         """
         Making a critic block
 
         Args:
             input_dim (int): the number of the input channels
-            output_chan (int): the number of the output channels
+            output_dim (int): the number of the output channels
             kernel_size (int): the size of the kernel
             stride (int): the stride of the convolution
             padding (int): the padding of the convolution
@@ -32,13 +37,13 @@ class Critic(nn.Module):
         """
         if not final_layer:
             return nn.Sequential(
-                nn.Conv2d(input_dim, output_chan, kernel_size, stride, padding),
-                nn.BatchNorm2d(output_chan),
+                nn.Conv2d(input_dim, output_dim, kernel_size, stride, padding),
+                nn.BatchNorm2d(output_dim),
                 nn.LeakyReLU(0.2, inplace=True)
             )
         else:
             return nn.Sequential(
-                nn.Conv2d(input_dim, output_chan, kernel_size, stride, padding)
+                nn.Conv2d(input_dim, output_dim, kernel_size, stride, padding)
             )
 
     def forward(self, img):
@@ -50,3 +55,11 @@ class Critic(nn.Module):
         """
         critic_pred = self.critic(img)
         return critic_pred.view(len(critic_pred), -1)
+
+
+if __name__ == '__main__':
+    # Testing the Critic model
+    img = torch.randn(1, 1, 32, 32)
+    critic = Critic()
+    critic_pred = critic(img)
+    print(critic_pred.shape)
--- src/wgan/Generator.py
+++ src/wgan/Generator.py
@@ -17,19 +17,19 @@ class Generator(nn.Module):
 
         # The Network
         self.gen = nn.Sequential(
-            self.make_gen_block(z_dim, 4*hidden_dim, padding=0),
-            self.make_gen_block(4*hidden_dim, 2*hidden_dim),
-            self.make_gen_block(2*hidden_dim, hidden_dim),
-            self.make_gen_block(hidden_dim, img_chan, final_layer=True)
+            self.make_gen_block(z_dim, 4*hidden_dim),
+            self.make_gen_block(4*hidden_dim, 2*hidden_dim, padding=1),
+            self.make_gen_block(2*hidden_dim, hidden_dim, padding=1),
+            self.make_gen_block(hidden_dim, img_chan, padding=1, final_layer=True)
         )
 
-    def make_gen_block(self, input_dim, img_chan, kernel_size=4, stride=2, padding=1, final_layer=False):
+    def make_gen_block(self, input_dim, output_dim, kernel_size=4, stride=2, padding=0, final_layer=False):
         """
         Making a generator block
 
         Args:
             input_dim (int): the number of the input channels
-            img_chan (int): the number of the output channels
+            output_dim (int): the number of the output channels
             kernel_size (int): the size of the kernel
             stride (int): the stride of the t-convolution
             padding (int): the padding of the t-convolution
@@ -37,13 +37,14 @@ class Generator(nn.Module):
         """
         if not final_layer:
             return nn.Sequential(
-                nn.ConvTranspose2d(input_dim, img_chan, kernel_size, stride, padding),
-                nn.BatchNorm2d(img_chan),
+                nn.ConvTranspose2d(input_dim, output_dim, kernel_size=kernel_size, stride=stride, padding=padding),
+                nn.BatchNorm2d(output_dim),
                 nn.ReLU()
             )
         else:
             return nn.Sequential(
-                nn.ConvTranspose2d(input_dim, img_chan, kernel_size, stride, padding)
+                nn.ConvTranspose2d(input_dim, output_dim, kernel_size=kernel_size, stride=stride, padding=padding),
+                nn.Tanh()
             )
 
     def unsqueeze_noise(self, z):
@@ -62,6 +63,7 @@ class Generator(nn.Module):
         Args:
             z (torch.Tensor): the z vector
         """
+        z = self.unsqueeze_noise(z)
         return self.gen(z)
 
 def make_noise(batch_size, z_dim, device='cpu'):
@@ -73,3 +75,11 @@ def make_noise(batch_size, z_dim, device='cpu'):
         z_dim (int): dimension of the noise vector
     """
     return torch.randn(batch_size, z_dim, device=device)
+
+
+if __name__ == '__main__':
+    # Testing the Generator class
+    gen = Generator(z_dim=10, img_chan=1, hidden_dim=64)
+    z = make_noise(batch_size=2, z_dim=10)
+    gen_pred = gen(z)
+    print(gen_pred.shape)
