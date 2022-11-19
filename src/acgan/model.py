6c0f099ece89ae246238e24082fd2998b168c404
Amir Buchali <amir.buchali@gmail.com>
2022-11-19 21:45:54 +0330

    Add: cross validation

--- src/acgan/model.py
+++ src/acgan/model.py
@@ -5,15 +5,20 @@ from src.acgan.utils import (combine_vectors, load_dataset, make_label,
                              make_noise, make_one_hot_labels, plot_tensor)
 from src.data import DATA_DIR
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
+from sklearn.model_selection import KFold
 
 
 class Model():
-    def __init__(self):
+    """
+    A class for training and testing the acgan network.
+    """
+    def __init__(self, n_classes=2, z_dim=20, n_repeats=2):
 
-        self.n_classes = 2
-        self.z_dim = 20
-        self.n_repeats = 2
+        st.title('ACGAN')
+        self.n_classes = n_classes
+        self.z_dim = z_dim
+        self.n_repeats = n_repeats
 
         self.load_model()
         self.create_optimizers()
@@ -34,21 +39,43 @@ class Model():
         lr_gen=0.002, lr_critic=0.001, lr_classifier=0.001,
         beta_1=0.5, beta_2=0.999
         ):
+        """
+        Create and initials the adam optimizers for the networks.
 
+        Args:
+            lr_gen (float, optional): generator learning rate. Defaults to 0.002.
+            lr_critic (float, optional): critic learning rate. Defaults to 0.001.
+            lr_classifier (float, optional): classifier learning rate. Defaults to 0.001.
+            beta_1 (float, optional): momentum beta 1 param. Defaults to 0.5.
+            beta_2 (float, optional): momentum beta 2. Defaults to 0.999.
+        """
         ## create optimizers
         self.gen_opt = torch.optim.Adam(params=self.gen.parameters(), lr=lr_gen, betas=(beta_1, beta_2))
         self.classifier_opt = torch.optim.Adam(params=self.classifier.parameters(), lr=lr_classifier, betas=(beta_1, beta_2))
         self.critic_opt = torch.optim.Adam(params=self.critic.parameters(), lr=lr_critic, betas=(beta_1, beta_2))
 
     def init_weights(self):
+        """
+        Initialize the networks weights.
+        """
         # apply init values
         self.gen = self.gen.apply(weights_init)
         self.classifier = self.classifier.apply(weights_init)
         self.critic = self.critic.apply(weights_init)
 
     def train(self, train_dataset, batch_size=28, epochs=30, display_step=30, device='cpu'):
+        """
+        Train the model based on train_dataset.
 
-        dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=None)
+        Args:
+            train_dataset: Training dataset.
+            batch_size (int, optional): batch size. Defaults to 28.
+            epochs (int, optional): number of epochs. Defaults to 30.
+            display_step (int, optional): display the progress on display_step. Defaults to 30.
+            device (str, optional): computation device. Defaults to 'cpu'.
+        """
+
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=None)
         self.init_weights()
 
         # Training loop
@@ -58,9 +85,8 @@ class Model():
         critic_loss_mean = 0
         classifier_mean_error = 0
 
-        st.title('ACGAN Training')
         for epoch in range(epochs):
-            for real, labels in dataloader:
+            for real, labels in train_dataloader:
                 cur_batch_size = len(real)
                 real = real.to(device=device)
 
@@ -143,7 +169,58 @@ class Model():
                     classifier_loss_mean = 0
                     critic_loss_mean = 0
                     classifier_mean_error = 0
-        st.success('Training Completed Successfully!', icon="✅")
+
+
+    def test(self, test_dataset):
+        """
+        Test the model based on test_dataset.
+
+        Args:
+            test_dataset: Test dataset.
+
+        Returns:
+            Accuracy(float): Accuracy of the model on test dataset.
+        """
+        test_dataloader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=None)
+        with torch.no_grad():
+            for real, targets in test_dataloader:
+                preds, _ = self.classifier(real)
+                err = abs(preds - targets).sum() / len(real)
+
+                return (1 - err) * 100
+
+    def cross_validate(self, dataset, k_folds=5):
+        """
+        K-fold cross validation of the model on dataset.
+
+        Args:
+            dataset: the dataset to test and train.
+            k_folds (int, optional): number of folds. Defaults to 5.
+        """
+        # Define the K-fold Cross Validator
+        kfold = KFold(n_splits=k_folds, shuffle=True)
+
+        accuracy = {}
+        # K-fold Cross Validation model evaluation
+        for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
+            st.write(f'fold: {fold + 1} / {k_folds}')
+
+            # train test dataset split
+            train_subset = Subset(dataset, train_ids, )
+            test_subset = Subset(dataset, test_ids)
+
+            st.write(f"{10 * '_'}")
+            st.header('Training')
+            self.train(train_subset)
+
+            st.header('Testing')
+            acc = self.test(test_subset)
+            accuracy[f'Fold: {fold + 1}'] = acc
+            st.write(f'Fold {fold + 1} Accuracy: {acc:.2f} %')
+
+        mean_accuracy = sum(accuracy.values()) / k_folds
+        st.success('Test Completed Successfully!', icon="✅")
+        st.write(f'Mean Accuracy: {mean_accuracy:.2f} %')
 
 
 if __name__ == '__main__':
@@ -152,4 +229,4 @@ if __name__ == '__main__':
     graz_dataset = load_dataset(root)
 
     gan_model = Model()
-    gan_model.train(graz_dataset)
+    gan_model.cross_validate(graz_dataset)
